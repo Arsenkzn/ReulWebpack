102ed094f4bbbb3c217c31905051acecdbb2e955
Arsenkzn <archi1504@gmail.com>
2025-02-16 11:11:14 +0300

    сделал prod и dev на webpack

---
+++ src/index.js
@@ -0,0 +1,51 @@
+const volumeSlider = document.getElementById('volumeSlider');
+    const buttons = document.querySelectorAll('.buttons button');
+    const summerAudio = document.getElementById('summerAudio');
+    const rainAudio = document.getElementById('rainAudio');
+    const winterAudio = document.getElementById('winterAudio');
+    let currentAudio = null;
+
+    // Volume control
+    volumeSlider.addEventListener('input', function() {
+        summerAudio.volume = this.value;
+        rainAudio.volume = this.value;
+        winterAudio.volume = this.value;
+    });
+
+    // Play/Pause and background update
+    buttons.forEach(button => {
+        button.addEventListener('click', function() {
+            const sound = this.dataset.sound;
+            let audio;
+
+            switch (sound) {
+                case 'summer':
+                    audio = summerAudio;
+                    document.body.className = 'summer-bg';
+                    break;
+                case 'rain':
+                    audio = rainAudio;
+                    document.body.className = 'rain-bg';
+                    break;
+                case 'winter':
+                    audio = winterAudio;
+                    document.body.className = 'winter-bg';
+                    break;
+            }
+
+            if (currentAudio === audio) {
+                if (audio.paused) {
+                    audio.play();
+                } else {
+                    audio.pause();
+                }
+            } else {
+                if (currentAudio) {
+                    currentAudio.pause();
+                    currentAudio.currentTime = 0;
+                }
+                audio.play();
+                currentAudio = audio;
+            }
+        });
+    });
