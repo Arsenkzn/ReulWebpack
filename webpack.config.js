c925644065d6d49f93c743dad8d73bda88dd532d
Arsenkzn <archi1504@gmail.com>
2025-02-16 11:37:21 +0300

    сделал html plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
     context: path.resolve(__dirname, 'src'),
@@ -7,5 +8,8 @@ module.exports = {
         filename: '[name].[contenthash].js',
         path: path.resolve(__dirname, 'dist'),
         clean: true,
-    }
+    },
+    plugins: [new HtmlWebpackPlugin({
+        template: path.resolve(__dirname, 'public/index.html')
+    })],
 }
