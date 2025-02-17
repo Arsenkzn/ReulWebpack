616430bbd09f685d314c0791989e9862e5e7588d
Arsenkzn <archi1504@gmail.com>
2025-02-17 14:35:56 +0300

    optimization

--- webpack.dev.js
+++ webpack.dev.js
@@ -4,4 +4,9 @@ const commonConfig = require('./webpack.config')
 module.exports = merge(commonConfig, {
     mode: 'development',
     devtool: 'inline-source-map',
+    devServer: {
+        port: 5500, 
+        hot: true,
+        open: true,
+    }
 })
