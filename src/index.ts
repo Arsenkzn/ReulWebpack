334a50dd6865c2883dce1bf6680413ee365dd871
Arsenkzn <archi1504@gmail.com>
2025-02-24 11:59:37 +0300

    релиз

---
+++ src/index.ts
@@ -0,0 +1,52 @@
+//import css from "file.css"
+
+    const volumeSlider = document.getElementById('volumeSlider') as HTMLInputElement;
+    const buttons = document.querySelectorAll('.buttons button') as NodeListOf<HTMLButtonElement>;
+    const summerAudio = document.getElementById('summerAudio') as HTMLAudioElement;
+    const rainAudio = document.getElementById('rainAudio') as HTMLAudioElement;
+    const winterAudio = document.getElementById('winterAudio') as HTMLAudioElement;
+    let currentAudio: HTMLAudioElement | null = null;
+
+
+    volumeSlider.addEventListener('input', function() {
+        summerAudio.volume = parseFloat(this.value);
+        rainAudio.volume = parseFloat(this.value);
+        winterAudio.volume = parseFloat(this.value);
+    });
+
+    buttons.forEach(button => {
+        button.addEventListener('click', function(): void {
+            const sound = this.dataset.sound;
+            let audio: HTMLAudioElement;
+
+            switch (sound) {
+                case 'summer':
+                    audio = summerAudio;
+                    document.body.className = 'summer-bg';
+                    break;
+                case 'rain':
+                    audio = rainAudio;
+                    document.body.className = 'rain-bg';
+                    break;
+                case 'winter':
+                    audio = winterAudio;
+                    document.body.className = 'winter-bg';
+                    break;
+            }
+
+            if (currentAudio === audio) {
+                if (audio.paused) {
+                    audio.play();
+                } else {
+                    audio.pause();
+                }
+            } else {
+                if (currentAudio) {
+                    currentAudio.pause();
+                    currentAudio.currentTime = 0;
+                }
+                audio.play();
+                currentAudio = audio;
+            }
+        });
+    });
